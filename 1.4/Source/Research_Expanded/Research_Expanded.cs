8923e0108f2f0d5f6ffb3280b7ebe2726a20aca3
Dexter Lowe <feldoh10@gmail.com>
2023-07-15 01:50:18 +0100

    Initial CSharp Scaffolding

---
+++ 1.4/Source/Research_Expanded/Research_Expanded.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Verse;
+
+namespace Research_Expanded
+{
+    public class Mod : Verse.Mod
+    {
+        public static Settings settings;
+
+        public Mod(ModContentPack content) : base(content)
+        {
+            Log.Message("Hello world from Research Expanded");
+
+            // initialize settings
+            settings = GetSettings<Settings>();
+
+#if DEBUG
+			Harmony.DEBUG = true;
+#endif
+
+            Harmony harmony = new Harmony("TheCatLover366.rimworld.Research_Expanded.main");
+            harmony.PatchAll();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            settings.DoWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Research Expanded";
+        }
+    }
+}
